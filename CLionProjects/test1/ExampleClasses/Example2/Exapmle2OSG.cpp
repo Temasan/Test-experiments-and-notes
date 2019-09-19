71cd556abec491a223107e91923f9c361458e500
Temasan <savin_ar@mail.ru>
2019-09-19 23:55:26 +0300

    Load multiple View OSG

--- CLionProjects/test1/ExampleClasses/Example2/Exapmle2OSG.cpp
+++ CLionProjects/test1/ExampleClasses/Example2/Exapmle2OSG.cpp
@@ -5,12 +5,16 @@
 #include "Exapmle2OSG.h"
 #include <osgDB/ReadFile>
 #include <osgViewer/Viewer>
+#include <osgWidget/Widget>
 
 Exapmle2OSG::Exapmle2OSG() = default;
 
 int Exapmle2OSG::set3DElement(){
     m_root = osgDB::readNodeFile("../data/Faucet LUMIER LM1808 N200416.3DS");
-    osgViewer::Viewer viewer;
-    viewer.setSceneData(m_root.get());
-    return viewer.run();
+    osg::ref_ptr<osgViewer::View> view = new osgViewer::View;
+    view->setSceneData(m_root);
+    view->setUpViewInWindow(50, 50, 320, 240);
+    osgViewer::CompositeViewer multiviewer;
+    multiviewer.addView(view);
+    return multiviewer.run();
 }
--- CLionProjects/test1/ExampleClasses/Example2/Exapmle2OSG.h
+++ CLionProjects/test1/ExampleClasses/Example2/Exapmle2OSG.h
@@ -7,13 +7,18 @@
 #include <osg/Geode>
 #include <osg/Node>
 #include <osgDB/ReaderWriter>
+#include <osgWidget/Frame>
+#include <memory>
 
 class Exapmle2OSG {
 public:
     explicit Exapmle2OSG();
+    ~Exapmle2OSG();
     int set3DElement();
 private:
     osg::ref_ptr<osg::Node> m_root;
+    std::unique_ptr<osgWidget::Frame> m_wdgt;
+    osg::DisplaySettings *ds;
 };
 
 
